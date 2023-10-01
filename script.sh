89dc2cadbd21aa325a598d35134d7739c07b667d
Muhammad Afaq Nasir <48288031+AfaqNasir@users.noreply.github.com>
2023-10-01 14:31:44 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Access the secret variable
-echo $MY_SECRET | base64 --decode
+echo $MY_SECRET | base64 -d ; echo
 
 # Use the secret variable in your script
 # Your script logic here
