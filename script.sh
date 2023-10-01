798a52547c6f72367e5e9407f2d03afa4e862bb6
Muhammad Afaq Nasir <48288031+AfaqNasir@users.noreply.github.com>
2023-10-01 13:45:42 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,6 @@
 # Access the secret variable
 echo "My secret is: $MY_SECRET"
 
+echo "My decrypted secret is: $MY_SECRET_DECRYPTED"
 # Use the secret variable in your script
 # Your script logic here
